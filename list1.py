6e0b3e3280f767cf60ac86479ec18086132f046c
Andrew Canter <andrew.canter.5@gmail.com>
2020-08-06 12:11:41 -0400

    Testing pull request

--- list1.py
+++ list1.py
@@ -6,7 +6,7 @@ Kenzie assignment: List1
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
 __author__ = "Andrew Canter"
-
+#Testing
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
